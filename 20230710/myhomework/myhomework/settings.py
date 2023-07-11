fb9f284e0a671554b6c8319d9a7471d5a7823fbc
lxmliu2002 <lxmliu2002@126.com>
2023-07-11 10:14:23 +0800

    20230710

--- 20230710/myhomework/myhomework/settings.py
+++ 20230710/myhomework/myhomework/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'myhomework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myhomework',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
+
 
 
 # Password validation
